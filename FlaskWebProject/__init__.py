5a831bf65ddf17dbb70f962bf6fe874a5be19607
KhanhHoang111 <43487666+KhanhHoang111@users.noreply.github.com>
2024-05-02 21:19:58 +0700

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,9 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://azureuser:Kkhanhss#154@sqlserverprj1.database.windows.net/SQLdatabase?driver=ODBC+Driver+17+for+SQL+Server'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
