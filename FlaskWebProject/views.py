bcd6d8fb17b55242d9350f15cc60e79e729b944a
KhanhHoang111 <43487666+KhanhHoang111@users.noreply.github.com>
2024-05-02 21:19:28 +0700

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,10 +1,10 @@
-"""
-Routes and views for the flask application.
-"""
 
 from datetime import datetime
 from flask import render_template, flash, redirect, request, session, url_for
-from werkzeug.urls import url_parse
+
+# Remove this pakage
+# from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from config import Config
 from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
@@ -19,6 +19,18 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 @app.route('/home')
 @login_required
 def home():
+
+    log = "info"
+    if log:
+        if log == 'info':
+            app.logger.info('No issue.')
+        elif log == 'warning':
+            app.logger.warning('Warning occurred.')
+        elif log == 'error':
+            app.logger.error('Error occurred.')
+        elif log == 'critical':
+            app.logger.critical('Critical error occurred.')
+    
     user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
     return render_template(
@@ -66,11 +78,15 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            print("User invalid username or password")
+            app.logger.info("User invalid username or password")
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        print("User login successful")
+        app.logger.info("User login successful")
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
@@ -85,20 +101,24 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
-        request.args['code'],
-        scopes=Config.SCOPE,
-        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
-        #  And replace the error dictionary
-        result = {'error': 'Not Implemented', 'error_description': 'Function not implemented.'}        
+        # TODO:DONE Acquire a token from a built msal app, along with the appropriate redirect URI
+        result = _build_msal_app( cache=cache).acquire_token_by_authorization_code(
+            request.args.get('code'),
+            scopes=Config.SCOPE,
+            redirect_uri=url_for('authorized', _external=True, _scheme='https')
+        )
+
         if "error" in result:
+            print("User login failed")
+            app.logger.info("User login failed")
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
+        print("User login successful")
         login_user(user)
+        app.logger.info("User login successful")
         _save_cache(cache)
     return redirect(url_for('home'))
 
@@ -116,26 +136,30 @@ def logout():
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
+    # TODO:DONE Load the cache from `msal`, if it exists
     cache = msal.SerializableTokenCache()
     if session.get('token_cache'):
         cache.deserialize(session['token_cache'])
-    return cache    
+    return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
+    # TODO:DONE Save the cache, if it has changed
     if cache.has_state_changed:
         session['token_cache'] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
+    # TODO:DONE Return a ConfidentialClientApplication
     return msal.ConfidentialClientApplication(
-        Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
-        client_credential=Config.CLIENT_SECRET, token_cache=cache)
+        client_id=Config.CLIENT_ID,
+        client_credential=Config.CLIENT_SECRET,
+        authority=authority or Config.AUTHORITY,
+        token_cache=cache, 
+    )
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes or [],
-        state=state or str(uuid.uuid4()),
-        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    # TODO:DONE Return the full Auth Request URL with appropriate Redirect URI
+    return _build_msal_app( authority=authority ).get_authorization_request_url(
+        scopes,
+        state=state or 'default_state',
+        redirect_uri=url_for('authorized', _external=True, _scheme='https')
+    )
